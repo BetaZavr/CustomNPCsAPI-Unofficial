cebcda2df1c9417958a29f13d03c25baced26e17
BetaZavr <75635959+BetaZavr@users.noreply.github.com>
2024-05-27 09:17:04 +0300

    Create IBlock.java

---
+++ noppes/npcs/api/block/IBlock.java
@@ -0,0 +1,66 @@
+package noppes.npcs.api.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import noppes.npcs.api.IContainer;
+import noppes.npcs.api.INbt;
+import noppes.npcs.api.IPos;
+import noppes.npcs.api.IWorld;
+import noppes.npcs.api.entity.data.IData;
+
+public interface IBlock {
+
+	void blockEvent(int type, int data);
+
+	IContainer getContainer();
+
+	String getDisplayName();
+
+	Block getMCBlock();
+
+	IBlockState getMCBlockState();
+
+	TileEntity getMCTileEntity();
+
+	int getMetadata();
+
+	String getName();
+
+	IPos getPos();
+
+	IData getStoreddata();
+
+	IData getTempdata();
+
+	INbt getTileEntityNBT();
+
+	IWorld getWorld();
+
+	int getX();
+
+	int getY();
+
+	int getZ();
+
+	boolean hasTileEntity();
+
+	void interact(int side);
+
+	boolean isAir();
+
+	boolean isContainer();
+
+	boolean isRemoved();
+
+	void remove();
+
+	IBlock setBlock(IBlock block);
+
+	IBlock setBlock(String name);
+
+	void setMetadata(int i);
+
+	void setTileEntityNBT(INbt nbt);
+
+}
